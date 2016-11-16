daee58e54e20cde6f3be105dd1fa96e3ded22c48
Piotr Waszkiewicz <waszka23@gmail.com>
2016-11-17 00:03:10 +0100

    Some new code 3

--- Prolog/Program/Wersja2/animals.pl
+++ Prolog/Program/Wersja2/animals.pl
@@ -1,4 +1,4 @@
-zwierze(panda, [1], []).
+zwierze(panda, [1, 2], []).
 zwierze(waszka, [2], []).
 zwierze(krokodyl, [3], []).
 
--- Prolog/Program/Wersja2/read_database.pl
+++ Prolog/Program/Wersja2/read_database.pl
@@ -5,6 +5,12 @@ read_questions(Questions):-
 	delete(Q, Questions),
 	close(Str).
 
+read_animals(Animals):-
+	open('animals.pl',read,Str),
+	read_database(Str,A),
+	delete(A,Animals),
+	close(Str).
+
 read_database(Stream,[]):-
 	at_end_of_stream(Stream).
 
@@ -27,6 +33,10 @@ list_questions():-
 	read_questions(Questions),
 	display_questions(Questions, 1).
 
+list_animals():-
+	read_animals(Animals),
+	display_questions(Animals, 1).
+
 display_questions([], _).
 display_questions([Q1|[Q]], Number):-
 	write(Number), write('. '),
@@ -34,7 +44,10 @@ display_questions([Q1|[Q]], Number):-
 	N is Number+1,
 	display_questions(Q, N).
 
-% Get question info
+% Get question info	
+get_question_text(Number, Text):-
+	[questions],
+	pytanie(_, Number, Text).
 get_question_text(Predicate, Text):-
 	[questions],
 	pytanie(Predicate, _, Text).
@@ -42,3 +55,28 @@ get_question_text(Predicate, Text):-
 get_question_number(Predicate, Number):-
 	[questions],
 	pytanie(Predicate, Number, _).
+
+% Start quiz
+start_quiz():-
+	[getyesno],
+	read_animals(Animals),
+	traverse_and_ask_question(Animals).
+
+% Go through all animals and ask questions regarding each one of them
+traverse_and_ask_question([]).
+traverse_and_ask_question([Animal | [Rest]]):-
+	ask_questions(Animal),
+	traverse_and_ask_question(Rest).
+
+% Ask all questions regarding your animals
+ask_questions(zwierze(Name, Questions, _)):-
+	ask_question(Questions, Name).
+
+% Ask one specific question
+ask_question([], Name):-
+	write('Twoim zwierzeciem jest '), write(Name), nl.
+ask_question([Qnum | Rest], Name):-
+	get_question_text(Qnum, Text),
+	write(Text), nl,
+	get_yes_or_no(_),
+	ask_question(Rest, Name).
