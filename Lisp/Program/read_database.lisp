015bb0b4a88949b6ac8c9e5deebe9ce11025d5bd
Piotr Waszkiewicz <waszka23@gmail.com>
2017-01-04 20:26:54 +0100

    Added question traversal

--- Lisp/Program/read_database.lisp
+++ Lisp/Program/read_database.lisp
@@ -46,6 +46,20 @@
     )
 )
 
+(defmacro ask (question_text and expect answer_text)
+    (let ((question (gensym)) (answer (gensym)) (user_answer (gensym)))
+        `(let ((question ,question_text) (answer ,answer_text))
+            (print question)
+            (setq user_answer (string-downcase (write-to-string (read))))
+            (if (not (equal user_answer answer))
+                ;; TODO: save answer miss :(
+                (print "Nie tego sie spodziewalem! :(")
+            )
+        )
+    )
+    ;; TODO: Return miss or hit info
+)
+
 (defmacro ask_questions (for questions_number in questions_list using question_data expect expected_answer)
     (let ((questions (gensym)) (question_dict (gensym)) (expected (gensym)))
         `(let ((questions ,questions_list) (question_dict ,question_data) (expected ,expected_answer))
@@ -54,8 +68,8 @@
                     do (setq qnum (nth 0 d))
                     do (setq qtxt (nth 1 d))
                     do (if (equal q qnum)
-                         ;; Wczytywanie odpowiedzi!
-                        (print (concatenate 'string qtxt " expected: " expected))
+                        ;; TODO: Save answers!
+                        (ask qtxt and expect expected)
                     )
                 )
             )
@@ -70,13 +84,25 @@
     (ask_questions for q in no_questions using questions expect "no")
 )
 
-;; TODO: Remove this after testing!
-(defvar animals (read_animals))
-(defvar questions (read_questions))
-(print animals)
-(print questions)
-(save_database animals "list.lisp")
+(defmacro foreach (element in list apply function with parameters)
+    ;; Macro for applying function to each element in list with other optional parameters
+    ;; function parameter has to be provided in form of function hadle
+    ;; e.g. #'function_name
 
-(ask_about (nth 0 animals) questions)
-(ask_about (nth 1 animals) questions)
+    (let ((element_list (gensym)) (fun (gensym)) (params (gensym)))
+        `(let ((element_list ,list) (fun ,function) (params ,parameters))
+            (loop for e in element_list
+                do (funcall fun e params)
+            )
+        )
+    )
+)
 
+(defun quiz ()
+    (setq animals (read_animals))
+    (setq questions (read_questions))
+    ; (ask_about (nth 0 animals) questions)
+    (foreach animal in animals apply #'ask_about with questions)
+)
+
+(quiz)
